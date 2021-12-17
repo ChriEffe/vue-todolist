8ab25869881f456c5987dca0b5d9a34daa0c43d8
ChriEffe <christianferrazzano93@gmail.com>
2021-12-17 14:56:23 +0100

    milestone 1

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 Rifare l’esercizio della to do list.
 Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
 - text, una stringa che indica il testo del todo
-- done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+- done, un booleano (false/false) che indica se il todo è stato fatto oppure no
 MILESTONE 1
 Stampare all’interno di una lista, un item per ogni todo.
 Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
@@ -12,3 +12,27 @@ MILESTONE 3
 Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.*/
 
 
+const app = new Vue(
+    {
+        el: '#app',
+        data: {
+            todos: [
+                {
+                    text: 'Creare repo',
+                    done: false
+                },
+                {
+                    text: 'Pushare repo',
+                    done: false
+                },
+                {
+                    text: 'Aprire hotwell',
+                    done: false
+                }
+            ]
+        },
+        methods: {
+            
+        }
+    }
+)
